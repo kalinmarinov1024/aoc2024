deb2d0023d459f0a5e69dc339fa1211db57877a8
kalin1024 <kalinmarinov99@gmail.com>
2024-12-16 21:29:19 +0200

    Add solution for part 2

--- day1/historian-hysteria/src/main.rs
+++ day1/historian-hysteria/src/main.rs
@@ -1,14 +1,16 @@
 use std::fs;
+use std::collections::HashMap;
 
 fn main() {
     let path = "../input.txt";
 
-    let result = read_file_and_get_columns(&path).unwrap();
+    let part1 = part1(&path).unwrap();
+    let part2 = part2(&path).unwrap();
 
-    print!("{result}")
+    print!("{part2}");
 }
 
-fn read_file_and_get_columns(file_path: &str) -> Option<u32> {
+fn part1(file_path: &str) -> Option<u32> {
     let data = fs::read_to_string(file_path).expect("Error reading file");
 
     let (mut a, mut b) = (Vec::new(), Vec::new());
@@ -28,5 +30,29 @@ fn read_file_and_get_columns(file_path: &str) -> Option<u32> {
         .map(|(a, b)| a.abs_diff(*b))
         .sum(),
     )
+}
 
+fn part2(file_path: &str) -> Option<u32> {
+    let data = fs::read_to_string(file_path).expect("Error reading file");
+
+    let mut a= Vec::new();
+    let mut b = HashMap::new();
+
+    for line in data.lines() {
+        let mut parts = line.split("   ");
+
+        let left_column: u32 = parts.next().unwrap().parse::<u32>().unwrap();
+        let right_column: u32 = parts.next().unwrap().parse::<u32>().unwrap();
+
+        a.push(left_column);
+
+        let count: &mut u32 = b.entry(right_column).or_insert(0);
+        *count += 1;
+    }
+
+    Some(
+        a.iter()
+        .map(|a| a * b.get(a).unwrap_or(&0))
+        .sum()
+    )
 }
