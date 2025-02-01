b52ba7945237ea1228846bb348876e158be4855f
mkalin1024 <kalinmarinov99@gmail.com>
2025-02-01 18:27:21 +0200

    Day 2 solutions

--- day2/red-nosed/src/main.rs
+++ day2/red-nosed/src/main.rs
@@ -5,9 +5,10 @@ fn main() {
     
     let result_part_one = part1(input);
     println!("result: {}", result_part_one);
-
+    
+    let result_part_two = part2(input);
+    println!("result: {}", result_part_two);
 }
-
 
 fn part1(file_path: &str) -> u32 {
     let mut safe_reports: u32 = 0;
@@ -18,24 +19,58 @@ fn part1(file_path: &str) -> u32 {
         .map(|s| s.parse::<u32>().unwrap())
         .collect();
 
-        let mut prev: u32 = result[0];
-        let starting_direction: bool = result[0] > result[1];
-        let mut success: bool = false;
-
-        for x in &result[1..] {
-            let diff = prev.abs_diff(*x); 
-
-            if starting_direction == (prev > *x) && diff >= 1 && diff <=3 {
-                prev = *x;
-                success = true;
-            } else {
-                success = false;
-                break;
-            }    
+        if is_monotonic(&result) {
+            safe_reports += 1;
         }
-
-        if success {safe_reports += 1}
     }
 
     return safe_reports;
+}
+
+
+fn part2(file_path: &str) -> u32 {
+    let mut safe_reports: u32 = 0;
+    let data = fs::read_to_string(file_path).expect("Err reading file.");
+
+    for line in data.lines() {
+        let result: Vec<u32> = line.split_whitespace()
+        .map(|s| s.parse::<u32>().unwrap()).collect();
+
+        if is_monotonic_with_skip(&result) {
+            safe_reports += 1;
+        }
+    }
+
+    return safe_reports;
+}
+
+fn is_monotonic(numbers: &Vec<u32>) -> bool {
+    let mut prev: u32 = numbers[0];
+    let starting_direction: bool = numbers[0] > numbers[1];
+
+    for x in &numbers[1..] {
+        let diff = prev.abs_diff(*x);
+
+        if starting_direction == (prev > *x) && diff >= 1 && diff <=3 {
+            prev = *x;
+        } else {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+fn is_monotonic_with_skip(numbers: &Vec<u32>) -> bool {
+    let count = numbers.iter()
+    .filter(|_| {
+        is_monotonic(numbers) ||
+        (0..numbers.len()).any(|i| {
+            let mut numbers = numbers.clone();
+            numbers.remove(i);
+            is_monotonic(&numbers)
+            })
+    }).count();
+
+    return count != 0;
 }
